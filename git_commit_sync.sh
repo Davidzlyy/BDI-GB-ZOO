040cb9c906158d7bc43754e3bca7b743652f0ed5
David Hernandez <Davidsly829@gmail.com>
2024-10-30 20:37:05 -0500

    Add git_commit_sync.sh script for automating commit and sync process.

--- git_commit_sync.sh
+++ git_commit_sync.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Verifica el estado del repositorio
 git status
 
-# Solicita un mensaje de commit al usuario
+echo "Introduce el nombre de lo que quieres agregar"
+read add_name
+git add $add_name
+
 echo "Introduce el mensaje de commit:"
 read commit_message
 git commit -m "$commit_message"
 
-# Verifica el estado nuevamente
 git status
 
-# Sincroniza con la rama principal
 git pull origin main
 git push origin main
 
